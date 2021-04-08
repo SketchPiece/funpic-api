3625b0e3de8a4f2b3176e3299c39f2cc3866682e
SketchPiece <lubkin.andrey@gmail.com>
2021-04-08 16:06:18 +0300

    clean up

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/api', apiRoutes)
 
-// TODO: add docs
-// app.get('/', function (req, res) {
-//   res.send(
-//     'Добро пожаловать в Sketch-Api!\n/api/quote (принимает аргументы url,username,text)\n/api/trumpet (принимает аргументы urlfirst,urlsecond)\n/api/agree (принимает аргумент url)\n/api/enrages (принимает аргумент url)'
-//   )
-// })
-
 app.listen(PORT, () => {
   consola.success(`Funpic Api started on port ${PORT}`)
 })
